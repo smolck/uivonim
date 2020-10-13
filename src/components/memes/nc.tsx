24b2366dcd2885d5ccf03817931acd4a9e694bff
smolck <46855713+smolck@users.noreply.github.com>
2020-10-12 22:07:04 -0500

    Proper properties and format

--- src/components/memes/nc.tsx
+++ src/components/memes/nc.tsx
@@ -9,27 +9,31 @@ let state = {
 type S = typeof state
 
 const Nyancat = ({ visible }: S) => (
-    <div style={{
+  <div
+    style={{
       background: `url('../assets/nc.gif')`,
       display: visible ? 'block' : 'none',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '75vw',
       position: 'absolute',
       height: '100%',
       width: '100%',
-    }} />
+      'background-repeat': 'no-repeat',
+      'background-size': '75vw',
+    }}
+  />
 )
 
 const plugins = document.getElementById('plugins')
 const container = document.createElement('div')
 plugins?.appendChild(container)
 
-const show = () => (state.visible = true, render(<Nyancat {...state} />, container))
+const show = () => (
+  (state.visible = true), render(<Nyancat {...state} />, container)
+)
 const hide = () => {
-    if (state.visible) {
-        state.visible = false
-        render(<Nyancat {...state} />, container)
-    }
+  if (state.visible) {
+    state.visible = false
+    render(<Nyancat {...state} />, container)
+  }
 }
 
 api.onAction('nc', () => {
