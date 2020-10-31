0a13afafa85b8ad22ce6150d36d8cd6a09bfc2d2
smolck <46855713+smolck@users.noreply.github.com>
2020-10-30 23:09:12 -0500

    Nevermind on removing the ts-ignores

--- src/render/webgl-utils.ts
+++ src/render/webgl-utils.ts
@@ -98,6 +98,7 @@ const create = (options?: WebGLContextAttributes) => {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    // @ts-ignore typings are wrong, see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/pixelStorei#Pixel_storage_parameters
     gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true)
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas)
   }
