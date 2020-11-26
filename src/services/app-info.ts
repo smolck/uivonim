f0f71043659f0691259399bfac638adefc3c0055
smolck <46855713+smolck@users.noreply.github.com>
2020-11-26 09:03:24 -0600

    Remove deprecated

--- src/services/app-info.ts
+++ src/services/app-info.ts
@@ -6,17 +6,3 @@ api.onAction('version', () =>
 )
 
 api.onAction('devtools', () => remote.getCurrentWebContents().toggleDevTools())
-
-// TODO: DEPRECATED REMOVE
-api.onAction('remap-modifier', () => {
-  api.nvim.cmd(
-    `echo '"remap-modifier" is deprecated. please use "g:veonim_remap_modifiers"'`
-  )
-})
-
-// TODO: DEPRECATED REMOVE
-api.onAction('key-transform', () => {
-  api.nvim.cmd(
-    `echo '"key-transform" is deprecated. please use "g:veonim_key_transforms"'`
-  )
-})
