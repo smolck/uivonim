505a67f466e204f91509b86008e1fd9b6f97a4ee
smolck <46855713+smolck@users.noreply.github.com>
2021-03-08 07:53:07 -0600

    Prettier and more porting

--- src/render/webgpu.ts
+++ src/render/webgpu.ts
@@ -7,14 +7,14 @@ export default async (canvas: HTMLCanvasElement) => {
 
   const device = await adapter.requestDevice()
   if (!device) return
-  
-  const ctx = canvas.getContext('gpupresent') 
+
+  const ctx = canvas.getContext('gpupresent')
   if (!ctx) return
 
-  const swapChainFormat = await ctx.getSwapChainPreferredFormat(device);
+  const swapChainFormat = await ctx.getSwapChainPreferredFormat(device)
   const swapChain = ctx.configureSwapChain({
     device,
-    format: swapChainFormat
+    format: swapChainFormat,
   })
 
   const renderBufferStride = 4 * Float32Array.BYTES_PER_ELEMENT
@@ -33,27 +33,46 @@ export default async (canvas: HTMLCanvasElement) => {
           canvasResolution : vec2<f32>;
           fontAtlasResolution : vec2<f32>;
           colorAtlasResolution : vec2<f32>;
-
-          colorAtlasSampler : sampler;
-          colorAtlasTexture : texture2D;
+          cellSize : vec2<f32>;
+          cellPadding : vec2<f32>;
         }
-        `
+        [[binding(0), group(0)]] var<uniform> uniforms : Uniforms;
+
+        [[binding(1), group(0)]] var<uniform> colorAtlasSampler : sampler;
+        [[binding(2), group(0)]] var<uniform> colorAtlasTexture : texture2D;
+
+        [[binding(3), group(0)]] var<uniform> fontAtlasSampler : sampler;
+        [[binding(4), group(0)]] var<uniform> fontAtlasTexture : texture2D;
+
+        [[block]] struct CursorUniforms {
+          visible : bool;
+          position : vec2<f32>;
+          shape : i32;
+          color : vec4<f32>;
+        }
+        [[binding(5), group(0)]] var<uniform> cursor : CursorUniforms;
+
+        [[builtin(position)]] var<out> Position : vec4<f32>;
+
+        [[stage(vertex)]]
+        fn main() -> void {}
+        `,
       }),
-      entryPoint: 'main'
+      entryPoint: 'main',
     },
     fragmentStage: {
       module: device.createShaderModule({
         code: `
         `,
       }),
-      entryPoint: 'main'
+      entryPoint: 'main',
     },
     // TODO(smolck)
     primitiveTopology: 'triangle-list',
     colorStates: [
       {
-        format: swapChainFormat
-      }
+        format: swapChainFormat,
+      },
     ],
     vertexState: {
       vertexBuffers: [
@@ -65,10 +84,18 @@ export default async (canvas: HTMLCanvasElement) => {
             // cellPosition
             { shaderLocation: 1, offset: 0, format: 'float32x2' },
             // hlId
-            { shaderLocation: 2, offset: 2 * Float32Array.BYTES_PER_ELEMENT, format: 'float32' },
+            {
+              shaderLocation: 2,
+              offset: 2 * Float32Array.BYTES_PER_ELEMENT,
+              format: 'float32',
+            },
             // charIndex
-            { shaderLocation: 3, offset: 3 * Float32Array.BYTES_PER_ELEMENT, format: 'float32' },
-          ]
+            {
+              shaderLocation: 3,
+              offset: 3 * Float32Array.BYTES_PER_ELEMENT,
+              format: 'float32',
+            },
+          ],
         },
         // Quad Buffer
         {
@@ -78,27 +105,107 @@ export default async (canvas: HTMLCanvasElement) => {
           attributes: [
             {
               // quadVertex
-              shaderLocation: 0, offset: 0, format: 'float32x2'
-            }
-          ]
+              shaderLocation: 0,
+              offset: 0,
+              format: 'float32x2',
+            },
+          ],
         },
-      ]
+      ],
     },
     layout: device.createPipelineLayout({
       bindGroupLayouts: [
         device.createBindGroupLayout({
           entries: [
+            // uniforms
             {
               binding: 0,
               // TODO(smolck)
               visibility: GPUShaderStage.VERTEX,
               type: 'uniform-buffer',
               // TODO(smolck): minBufferBindingSize = ?
-            }
-          ]
-        })
-      ]
-    })
+            },
+            // colorAtlasSampler
+            { binding: 1, visibility: GPUShaderStage.VERTEX, type: 'sampler' },
+            // colorAtlasTexture
+            {
+              binding: 2,
+              visibility: GPUShaderStage.VERTEX,
+              type: 'sampled-texture',
+            },
+            // fontAtlasSampler
+            { binding: 3, visibility: GPUShaderStage.VERTEX, type: 'sampler' },
+            // fontAtlasTexture
+            {
+              binding: 4,
+              visibility: GPUShaderStage.VERTEX,
+              type: 'sampled-texture',
+            },
+            // cursor
+            {
+              binding: 5,
+              visibility: GPUShaderStage.VERTEX,
+              type: 'uniform-buffer',
+            },
+          ],
+        }),
+      ],
+    }),
+  })
+
+  const uniformBuffer = device.createBuffer({
+    // 5 vec2<f32>'s
+    size: 5 * 2 * 4,
+    // TODO(smolck)
+    usage: GPUBufferUsage.UNIFORM,
+  })
+
+  const cursorBuffer = device.createBuffer({
+    size:
+      4 /* shape : i32 */ +
+      2 * 4 /* position : vec2<f32> */ +
+      4 * 4 /* color : vec4<f32> */ +
+      4 /* visible: bool - is this size correct?*/,
+    // TODO(smolck)
+    usage: GPUBufferUsage.UNIFORM,
+  })
+
+  const bindGroup = device.createBindGroup({
+    layout: foregroundPipeline.getBindGroupLayout(0),
+    entries: [
+      // canvasResolution, fontAtlasResolution, colorAtlasResolution,
+      // cellSize, cellPadding - all vec2<f32>'s
+      {
+        binding: 0,
+        resource: {
+          buffer: uniformBuffer,
+        },
+      },
+      // colorAtlasSampler
+      {
+        binding: 1,
+        resource: device.createSampler({
+          minFilter: 'nearest',
+          magFilter: 'nearest',
+        }),
+      },
+      // colorAtlasTexture
+      // { binding: 2, resource: TODO }
+      // fontAtlasSampler
+      // TODO(smolck): See if a new one needs to be created
+      // or if the same one for the colorAtlas can be used here too
+      {
+        binding: 3,
+        resource: device.createSampler({
+          minFilter: 'nearest',
+          magFilter: 'nearest',
+        }),
+      },
+      // fontAtlasTexture
+      // { binding: 4, resource: TODO }
+      // cursor
+      { binding: 5, resource: cursorBuffer },
+    ],
   })
 
   const render = (buffer: Float32Array) => {
