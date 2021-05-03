914c573747ea391aa9a6356aa0baa3d4bfe46177
smolck <46855713+smolck@users.noreply.github.com>
2021-05-03 15:23:41 -0500

    Remove unnecessary code in dispatch.ts

--- src/renderer/dispatch.ts
+++ src/renderer/dispatch.ts
@@ -8,12 +8,10 @@ const dispatchConstructor = () => {
   return {
     pub: (event: string, ...args: any[]) => {
       if (event in listeners) {
-        console.log('pub event: ', event)
         listeners[event].forEach((cb: Cb) => cb(...args))
       }
     },
     sub: (event: string, cb: Cb) => {
-      console.log('sub event: ', event)
       if (event in listeners) {
         listeners[event].push(cb)
       } else {
@@ -35,25 +33,3 @@ export const sub = dispatch.sub
 export const unsub = dispatch.unsub
 export const pub = dispatch.pub
 export const processAnyBuffered = (_event: string) => {}
-
-/*const ee = new EventEmitter()
-
-const watchers = new Watchers()
-const cache = new Map<string, any[]>()
-const buffer = (event: string, data: any) =>
-  cache.has(event) ? cache.get(event)!.push(data) : cache.set(event, [data])
-
-export const unsub = (event: string, cb: Function) => watchers.remove(event, cb)
-export const sub = (event: string, cb: (...args: any[]) => void) =>
-  watchers.add(event, cb)
-
-export const pub = (event: string, ...args: any[]) => {
-  if (!watchers.has(event)) return buffer(event, args)
-  watchers.notify(event, ...args)
-}
-
-export const processAnyBuffered = (event: string) => {
-  if (!cache.has(event)) return
-  cache.get(event)!.forEach((d) => watchers.notify(event, ...d))
-  cache.delete(event)
-}*/
