63d358b247e1397efac5eda9b5092019d2d2492d
smolck <46855713+smolck@users.noreply.github.com>
2021-05-06 08:57:19 -0500

    parseGuifont tests

--- test/unit/common/utils.test.js
+++ test/unit/common/utils.test.js
@@ -32,6 +32,20 @@ describe('src/common/utils', () => {
     })
   }
 
+  describe('parseGuifont', () => {
+    const guifont = 'Iosevka Extrabold Italic:h20'
+
+    it('should parse the height correctly', () => {
+      const { size } = m.parseGuifont(guifont)
+      same(size, 20)
+    })
+
+    it('should parse the font name correctly', () => {
+      const { face } = m.parseGuifont(guifont)
+      same(face, 'Iosevka Extrabold Italic')
+    })
+  })
+
   describe('thread safe object', () => {
     it('convert getters to plain values', () => {
       class Position {
