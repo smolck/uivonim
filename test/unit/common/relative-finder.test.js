876f729226f6f08d90547428712c246a9b30ba2d
smolck <46855713+smolck@users.noreply.github.com>
2021-05-06 08:56:48 -0500

    Fix up tests

--- test/unit/common/relative-finder.test.js
+++ test/unit/common/relative-finder.test.js
@@ -1,4 +1,4 @@
-const { src, same } = require('../util')
+const { src, same } = require('../../util')
 const { findNext, findPrevious } = src('common/relative-finder')
 
 const getA = () => [
@@ -44,88 +44,90 @@ const getC = () => [
 
 const getItems = () => [...getA(), ...getC()]
 
-describe('relative finder', () => {
-  it('find next', () => {
-    const next = findNext(getItems(), '/main/a.ts', 2, 1)
+describe('src/common/relative-finder', () => {
+  describe('relative finder', () => {
+    it('find next', () => {
+      const next = findNext(getItems(), '/main/a.ts', 2, 1)
 
-    same(next, {
-      path: '/main/a.ts',
-      range: {
-        start: { line: 4, character: 7 },
-        end: { line: 4, character: 9 },
-      },
+      same(next, {
+        path: '/main/a.ts',
+        range: {
+          start: { line: 4, character: 7 },
+          end: { line: 4, character: 9 },
+        },
+      })
     })
-  })
 
-  it('find next file (and first item) when none in current', () => {
-    const next = findNext(getC(), '/main/a.ts', 9, 2)
+    it('find next file (and first item) when none in current', () => {
+      const next = findNext(getC(), '/main/a.ts', 9, 2)
 
-    same(next, {
-      path: '/main/c.ts',
-      range: {
-        start: { line: 1, character: 7 },
-        end: { line: 1, character: 9 },
-      },
+      same(next, {
+        path: '/main/c.ts',
+        range: {
+          start: { line: 1, character: 7 },
+          end: { line: 1, character: 9 },
+        },
+      })
     })
-  })
 
-  it('when last loopback to first', () => {
-    const next = findNext(getItems(), '/main/a.ts', 9, 2)
+    it('when last loopback to first', () => {
+      const next = findNext(getItems(), '/main/a.ts', 9, 2)
 
-    same(next, {
-      path: '/main/a.ts',
-      range: {
-        start: { line: 1, character: 1 },
-        end: { line: 1, character: 5 },
-      },
+      same(next, {
+        path: '/main/a.ts',
+        range: {
+          start: { line: 1, character: 1 },
+          end: { line: 1, character: 5 },
+        },
+      })
     })
-  })
 
-  it('find previous', () => {
-    const next = findPrevious(getItems(), '/main/a.ts', 2, 1)
+    it('find previous', () => {
+      const next = findPrevious(getItems(), '/main/a.ts', 2, 1)
 
-    same(next, {
-      path: '/main/a.ts',
-      range: {
-        start: { line: 1, character: 1 },
-        end: { line: 1, character: 5 },
-      },
+      same(next, {
+        path: '/main/a.ts',
+        range: {
+          start: { line: 1, character: 1 },
+          end: { line: 1, character: 5 },
+        },
+      })
     })
-  })
 
-  it('find prev file (and last item) when none is current', () => {
-    const next = findPrevious(getA(), '/main/c.ts', 1, 7)
+    it('find prev file (and last item) when none is current', () => {
+      const next = findPrevious(getA(), '/main/c.ts', 1, 7)
 
-    same(next, {
-      path: '/main/a.ts',
-      range: {
-        start: { line: 9, character: 2 },
-        end: { line: 9, character: 4 },
-      },
+      same(next, {
+        path: '/main/a.ts',
+        range: {
+          start: { line: 9, character: 2 },
+          end: { line: 9, character: 4 },
+        },
+      })
     })
-  })
 
-  it('when first loopback to last', () => {
-    const next = findPrevious(getItems(), '/main/a.ts', 1, 1)
+    it('when first loopback to last', () => {
+      const next = findPrevious(getItems(), '/main/a.ts', 1, 1)
 
-    same(next, {
-      path: '/main/a.ts',
-      range: {
-        start: { line: 9, character: 2 },
-        end: { line: 9, character: 4 },
-      },
+      same(next, {
+        path: '/main/a.ts',
+        range: {
+          start: { line: 9, character: 2 },
+          end: { line: 9, character: 4 },
+        },
+      })
     })
-  })
 
-  it('find previous when in middle of current item', () => {
-    const next = findPrevious(getItems(), '/main/a.ts', 4, 8)
+    it('find previous when in middle of current item', () => {
+      const next = findPrevious(getItems(), '/main/a.ts', 4, 8)
 
-    same(next, {
-      path: '/main/a.ts',
-      range: {
-        start: { line: 1, character: 1 },
-        end: { line: 1, character: 5 },
-      },
+      same(next, {
+        path: '/main/a.ts',
+        range: {
+          start: { line: 1, character: 1 },
+          end: { line: 1, character: 5 },
+        },
+      })
     })
   })
 })
